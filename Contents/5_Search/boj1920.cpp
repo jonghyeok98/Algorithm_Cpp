e6c3177b903611e7c106b1ea02bb0d3b53342ee4
이종혁 <gamza@ijonghyeog-ui-MacBookAir.local>
2023-11-06 18:04:27 +0900

    Add BinarySearch

---
+++ Contents/5_Search/boj1920.cpp
@@ -0,0 +1,56 @@
+// boj s4 1920
+// 수 찾기
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+static vector<int> v;
+
+bool binary_search(int l, int r, int num);
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    v.resize(N);
+
+    for(int i=0; i<N; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    int M;
+    cin >> M;
+    for(int i=0; i<M; i++)
+    {
+        int tmp;
+        cin >> tmp;
+        if(binary_search(0, v.size()-1, tmp))
+            cout << 1 << '\n';
+        else
+            cout << 0 << '\n';
+    }
+        
+    return 0;
+}
+
+bool binary_search(int l, int r, int num)
+{
+    if(r>=l)
+    {
+        int mid=(l+r)/2;
+        if(num==v[mid])
+            return true;
+        else if(num > v[mid])
+            return binary_search(mid+1, r, num);
+        else
+            return binary_search(l, mid-1, num);
+    }
+    return false;
+}
---
+++ Contents/5_Search/boj2343.cpp
@@ -0,0 +1,61 @@
+// boj s1 2343
+// 기타 레슨
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+static vector<int> v;
+int binary_search(int l, int r, int num);
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+
+    v.resize(N);
+    int left=0;
+    int right=0;
+    for(int i=0; i<N; i++)
+    {
+        cin >> v[i];
+        if(left<v[i])
+            left=v[i];
+        right+=v[i];
+    }
+    
+    int result = binary_search(left, right, M);
+    cout << result;
+
+    return 0;
+}
+
+int binary_search(int l, int r, int num)
+{
+    while(r>=l)
+    {
+        int count=1;
+        int size=0;
+        int mid=(l+r)/2;
+
+        for(int i=0; i<v.size(); i++)
+        {
+            if(size+v[i]>mid)
+            {
+                count++;
+                size=0;
+            }
+            size+=v[i];
+        }
+        if(num>=count)
+            r=mid-1;
+        else
+            l=mid+1;
+
+    }
+    return l;
+}
